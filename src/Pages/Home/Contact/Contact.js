f1ff083acf76f0d600c6d733d6355568cfa5fa22
saharabanu <saharabanungn@gmail.com>
2022-03-12 11:45:12 +0600

    email ja added

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
 import img from '../../../images/contact.png';
 
-const Contact = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        console.log(data)
 
-    };
+const Contact = () => {
+    
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('studypoint', 'template_a9erqi3', form.current, 'R_4-ZvT1bxgqne3x3')
+          .then((result) => {
+              alert('Message sent successfully')
+              console.log(result.text);
+          }, (error) => {
+              alert(error.message)
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
+   
 
 
     return (
@@ -17,11 +29,14 @@ const Contact = () => {
 
                 <div className="col-12 col-lg-6 col-md-6 col-sm-12 p-3 mt-5  shadow rounded-3 ">
                     <h3>Leave your message</h3>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <input type="text" {...register("name")} placeholder="Your name" className="form-control p-2 m-3 w-75 mx-auto bg-light" />
-                        <input type="email" {...register("email")} placeholder="example@email.com" className="form-control p-2 m-3 w-75 mx-auto bg-light" />
-                        <textarea {...register("message")} placeholder="leave your message" className="form-control p-2 m-3 w-75 mx-auto bg-light" style={{ height: "120px" }} />
-                        <button type="submit" className="btn btn-warning " ><i className="fas fa-paper-plane m-1"></i>Send</button>
+                    <form ref={form} onSubmit={sendEmail} >
+                        <input type="text"  placeholder="Your name" className=" p-2 m-3 w-75 mx-auto bg-light" required name="client-name" style={{outline:'none'}}/>
+
+                        <input type="email" placeholder="example@email.com" className=" p-2 m-3 w-75 mx-auto bg-light" name="email" required style={{outline:'none'}}/>
+
+                        <textarea  placeholder="leave your message" className="form-control p-2 m-3 w-75 mx-auto bg-light" name="message" required style={{ height: "120px",outline:'none' }}  />
+
+                        <button type="submit" value="send" className="btn btn-warning " ><i className="fas fa-paper-plane m-1"></i>Send</button>
 
                     </form>
                 </div>
